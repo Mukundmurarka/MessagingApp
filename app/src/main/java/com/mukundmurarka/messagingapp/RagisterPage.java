cc7225bdf1fe0ec8ebc2033e1dab1edeeadc4a0a
Mukundmurarka <mukundmurarka1996@gmail.com>
2018-09-25 11:07:18 +0530

    second

---
+++ app/src/main/java/com/mukundmurarka/messagingapp/RagisterPage.java
@@ -0,0 +1,13 @@
+package com.mukundmurarka.messagingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RagisterPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ragister_page);
+    }
+}
